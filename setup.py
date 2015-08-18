b2acc9663d7af200a4cdfcc809e9f65401153e48
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-08-18 14:16:43 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='signed-http-req',
     description='Python implementation of Signed HTTP Requests for OAuth.',
-    version='0.1',
+    version='1.0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=['pyjwkest'],
