58a7a5754504e280073a604ba6f4a0eca65323a8
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-08-18 13:56:48 +0200

    Updated interface and tests.

--- src/signed_http_req/__init__.py
+++ src/signed_http_req/__init__.py
@@ -2,14 +2,14 @@
 from base64 import urlsafe_b64encode
 import hashlib
 import json
+
 from jwkest import jws, BadSignature
 from jwkest.jws import JWS
-from jwkest.jwt import JWT
 
 __author__ = 'DIRG'
 
 
-class UnknownAlgError(Exception):
+class UnknownHashSizeError(Exception):
     pass
 
 
@@ -25,47 +25,125 @@ QUERY_PARAM_FORMAT = "{}={}"
 REQUEST_HEADER_FORMAT = "{}: {}"
 
 
-def sign_http(key, alg, method="", url_host="", path="", query_param=None,
-              req_header=None, req_body=None, time_stamp=""):
+def sign_http_request(key, alg, method="", host="", path="", query_params=None,
+                      headers=None, body=None, time_stamp=0):
+    """
+    Sign an HTTP request as a JWT.
+    :param key: signing key
+    :param alg: signing algorithm
+    :param method: HTTP method
+    :param host: url host
+    :param path: url path
+    :param query_params: query parameters
+    :param headers: HTTP headers
+    :param body: request body
+    :param time_stamp: timestamp
+    :return: signature of the request as a JWS
+    """
     http_json = {}
     hash_size = _get_hash_size(alg)
 
     if method:
-        method = method.upper()
-        http_json["m"] = method
+        http_json["m"] = method.upper()
 
-    if url_host:
-        http_json["u"] = url_host
+    if host:
+        http_json["u"] = host
 
     if path:
         http_json["p"] = path
 
-    if query_param:
-        param_keys, param_buffer = _serialize_dict(query_param,
+    if query_params:
+        param_keys, param_buffer = _serialize_dict(query_params,
                                                    QUERY_PARAM_FORMAT)
         param_hash = urlsafe_b64encode(
             _hash_value(hash_size, param_buffer)).decode("utf-8")
         http_json["q"] = [param_keys, param_hash]
 
-    if req_header:
-        header_keys, header_buffer = _serialize_dict(req_header,
+    if headers:
+        header_keys, header_buffer = _serialize_dict(headers,
                                                      REQUEST_HEADER_FORMAT)
         header_hash = urlsafe_b64encode(
             _hash_value(hash_size, header_buffer)).decode("utf-8")
         http_json["h"] = [header_keys, header_hash]
 
-    if req_body:
-        req_body = urlsafe_b64encode(req_body.encode("utf-8")).decode("utf-8")
-        http_json["b"] = req_body
+    if body:
+        body = urlsafe_b64encode(_hash_value(hash_size, body)).decode("utf-8")
+        http_json["b"] = body
 
     if time_stamp:
-        http_json["ts"] = time_stamp
+        http_json["ts"] = int(time_stamp)
 
     if not http_json:
         raise EmptyHTTPRequestError("No data to sign")
 
     jws = JWS(json.dumps(http_json), alg=alg)
     return jws.sign_compact(keys=[key])
+
+
+def verify_http_request(key, signature, method="", host="", path="",
+                        query_params=None, headers=None, body=None,
+                        strict_query_param_verification=False,
+                        strict_headers_verification=False):
+    """
+
+    :param key: verification key
+    :param signature: signature of the request
+    :param method: HTTP method
+    :param host: url host
+    :param path: url path
+    :param query_params: query parameters
+    :param headers: HTTP headers
+    :param body: request body
+    :param strict_query_param_verification:
+    :param strict_headers_verification:
+    :return:
+    """
+    _jw = jws.factory(signature)
+    if not _jw:
+        raise ValidationError("Not a signed request")
+
+    try:
+        unpacked_req = _jw.verify_compact(signature, keys=[key])
+        _header = _jw.jwt.headers
+        hash_size = _get_hash_size(_header["alg"])
+    except BadSignature:
+        raise ValidationError("Could not verify signature")
+
+    if "m" in unpacked_req:
+        _equals(unpacked_req["m"], method)
+    if "u" in unpacked_req:
+        _equals(unpacked_req["u"], host)
+    if "p" in unpacked_req:
+        _equals(unpacked_req["p"], path)
+
+    if "q" in unpacked_req:
+        param_keys, param_hash = unpacked_req["q"]
+        cmp_hash_str = "".join(
+            [QUERY_PARAM_FORMAT.format(k, query_params[k]) for k in
+             param_keys])
+        cmp_hash = urlsafe_b64encode(
+            _hash_value(hash_size, cmp_hash_str)).decode("utf-8")
+        _equals(cmp_hash, param_hash)
+        if strict_query_param_verification and len(param_keys) != len(
+                query_params):
+            raise ValidationError("Too many or too few query params")
+
+    if "h" in unpacked_req:
+        header_keys, header_hash = unpacked_req["h"]
+        cmp_hash_str = "".join(
+            [REQUEST_HEADER_FORMAT.format(k, headers[k]) for k in
+             header_keys])
+        cmp_hash = urlsafe_b64encode(
+            _hash_value(hash_size, cmp_hash_str)).decode("utf-8")
+        _equals(cmp_hash, header_hash)
+        if strict_headers_verification and len(header_keys) != len(headers):
+            raise ValidationError("Too many or too few headers")
+
+    if "b" in unpacked_req:
+        cmp_body = urlsafe_b64encode(_hash_value(hash_size, body)).decode("utf-8")
+        _equals(cmp_body, unpacked_req["b"])
+
+    return unpacked_req
 
 
 def _get_hash_size(alg):
@@ -81,67 +159,19 @@ def _hash_value(size, data):
     elif size == 512:
         return hashlib.sha512(data).digest()
 
-    raise UnknownAlgError("Unknown hash size: {}".format(size))
+    raise UnknownHashSizeError(str(size))
 
 
-def _serialize_dict(data, form):
+def _serialize_dict(data, serialization_template):
     buffer = []
     keys = []
     for key in data:
         keys.append(key)
-        buffer.append(form.format(key, data[key]))
+        buffer.append(serialization_template.format(key, data[key]))
 
     return keys, "".join(buffer)
 
 
-def verify_http(key, http_req, method="", url_host="", path="",
-                query_param=None,
-                req_header=None, req_body=None, strict_query_param=False,
-                strict_req_header=False):
-    _jw = jws.factory(http_req)
-    if _jw:
-        _jwt = JWT().unpack(http_req)
-        unpacked_req = _jwt.payload()
-        _header = _jwt.headers
-        try:
-            _jw.verify_compact(http_req, keys=[key])
-        except BadSignature:
-            raise ValidationError("Could not verify signature")
-
-        if "m" in unpacked_req:
-            _equal(unpacked_req["m"], method)
-        if "u" in unpacked_req:
-            _equal(unpacked_req["u"], url_host)
-        if "p" in unpacked_req:
-            _equal(unpacked_req["p"], path)
-        if "q" in unpacked_req:
-            param_keys, param_hash = unpacked_req["q"]
-            cmp_hash_str = "".join(
-                [QUERY_PARAM_FORMAT.format(k, query_param[k]) for k in
-                 param_keys])
-            cmp_hash = urlsafe_b64encode(
-                _hash_value(_get_hash_size(_header["alg"]),
-                            cmp_hash_str)).decode("utf-8")
-            _equal(cmp_hash, param_hash)
-            if strict_query_param and len(param_keys) != len(query_param):
-                raise ValidationError("To many or to few query params")
-        if "h" in unpacked_req:
-            header_keys, header_hash = unpacked_req["h"]
-            cmp_hash_str = "".join(
-                [REQUEST_HEADER_FORMAT.format(k, req_header[k]) for k in
-                 header_keys])
-            cmp_hash = urlsafe_b64encode(
-                _hash_value(_get_hash_size(_header["alg"]),
-                            cmp_hash_str)).decode("utf-8")
-            _equal(cmp_hash, header_hash)
-            if strict_req_header and len(header_keys) != len(req_header):
-                raise ValidationError("To many or to few headers")
-        if "b" in unpacked_req:
-            cmp_body = urlsafe_b64encode(req_body.encode("utf-8")).decode(
-                "utf-8")
-            _equal(cmp_body, unpacked_req["b"])
-
-
-def _equal(value_A, value_B):
-    if value_A != value_B:
-        raise ValidationError("{} != {}".format(value_A, value_B))
+def _equals(value, expected):
+    if value != expected:
+        raise ValidationError("{} != {}".format(value, expected))
--- tests/test_signed_http_req.py
+++ tests/test_signed_http_req.py
@@ -1,13 +1,15 @@
 # pylint: disable=missing-docstring
 from collections import Counter
+
 from jwkest.jwk import SYMKey
 import pytest
+
 from signed_http_req import _serialize_dict
 from signed_http_req import _get_hash_size
-from signed_http_req import UnknownAlgError
+from signed_http_req import UnknownHashSizeError
 from signed_http_req import _hash_value
-from signed_http_req import sign_http
-from signed_http_req import verify_http
+from signed_http_req import sign_http_request
+from signed_http_req import verify_http_request
 from signed_http_req import ValidationError
 from signed_http_req import EmptyHTTPRequestError
 
@@ -16,22 +18,15 @@ __author__ = 'DIRG'
 ALG = "HS256"
 SIGN_KEY = SYMKey(key="a_key", alg=ALG)
 
-METHOD = "GET"
-URL_HOST = "host"
-PATH = "/foo/bar"
-QUERY_PARAM = {"k1": "v1", "k2": "v2"}
-REQ_HEADER = {"h1": "d1", "h2": "d2"}
-REQ_BODY = "my body"
-TIME_STAMP = 12347456
-
-TEST_DATA = {"key": SIGN_KEY, "method": METHOD, "url_host": URL_HOST,
-             "path": PATH, "query_param": QUERY_PARAM, "req_header": REQ_HEADER,
-             "req_body": REQ_BODY}
+TEST_DATA = {"key": SIGN_KEY, "method": "GET", "host": "host",
+             "path": "/foo/bar", "query_params": {"k1": "v1", "k2": "v2"},
+             "headers": {"h1": "d1", "h2": "d2"},
+             "body": "my body"}
 
 
-def test_empty_http_req():
+def test_sign_empty_http_req():
     with pytest.raises(EmptyHTTPRequestError):
-        sign_http(SIGN_KEY, ALG)
+        sign_http_request(SIGN_KEY, ALG)
 
 
 def test_serialize():
@@ -43,14 +38,15 @@ def test_serialize():
 
     data_parts = serialized_data.split(".")[1:]
     for index, part in enumerate(data_parts):
-        key_value = part.split(":")
+        key, value = part.split(":")
 
-        assert key_value[0] == keys[index]
-        assert key_value[1] == data[key_value[0]]
+        assert key == keys[index]
+        assert value == data[key]
 
 
 @pytest.mark.parametrize("value,expected", [
     ("RS256", 256),
+    ("RS384", 384),
     ("RS512", 512),
 ])
 def test_get_hash_size(value, expected):
@@ -59,48 +55,55 @@ def test_get_hash_size(value, expected):
 
 def test_hash_value():
     data = "some_test_string"
-    with pytest.raises(UnknownAlgError):
+    with pytest.raises(UnknownHashSizeError):
         _hash_value(123, data)
     assert _hash_value(256, data)
 
 
 def test_verify():
-    result = sign_http(alg=ALG, time_stamp=TIME_STAMP, **TEST_DATA)
-    verify_http(http_req=result, **TEST_DATA)
+    timestamp = 12347456
+    result = sign_http_request(alg=ALG, time_stamp=12347456, **TEST_DATA)
+    signature = verify_http_request(signature=result, **TEST_DATA)
+
+    assert signature["ts"] == timestamp
 
 
 @pytest.mark.parametrize("key,value", [
     ("key", SYMKey(key="wrong_key", alg="HS256")),
     ("method", "FAIL"),
-    ("url_host", "FAIL"),
+    ("host", "FAIL"),
     ("path", "FAIL"),
-    ("query_param", {"k1": "v1", "k2": "FAIL"}),
-    ("req_header", {"h1": "d1", "h2": "FAIL"}),
-    ("req_body", "FAIL"),
+    ("query_params", {"k1": "v1", "k2": "FAIL"}),
+    ("headers", {"h1": "d1", "h2": "FAIL"}),
+    ("body", "FAIL"),
 ])
 def test_verify_fail(key, value, monkeypatch):
-    result = sign_http(alg=ALG, time_stamp=TIME_STAMP, **TEST_DATA)
+    result = sign_http_request(alg=ALG, **TEST_DATA)
     monkeypatch.setitem(TEST_DATA, key, value)
     with pytest.raises(ValidationError):
-        verify_http(http_req=result, **TEST_DATA)
+        verify_http_request(signature=result, **TEST_DATA)
 
 
 @pytest.mark.parametrize("key,value", [
-    ("query_param", {"k1": "v1", "k2": "v2", "k3": "v3"}),
-    ("req_header", {"h1": "d1", "h2": "d2", "h3": "d3"}),
+    ("query_params", {"k1": "v1", "k2": "v2", "k3": "v3"}),
+    ("headers", {"h1": "d1", "h2": "d2", "h3": "d3"}),
 ])
 def test_verify_strict(key, value, monkeypatch):
-    result = sign_http(alg=ALG, time_stamp=TIME_STAMP, **TEST_DATA)
+    result = sign_http_request(alg=ALG, **TEST_DATA)
     monkeypatch.setitem(TEST_DATA, key, value)
     with pytest.raises(ValidationError):
-        verify_http(http_req=result, strict_query_param=True, strict_req_header=True, **TEST_DATA)
+        verify_http_request(signature=result,
+                            strict_query_param_verification=True,
+                            strict_headers_verification=True, **TEST_DATA)
 
 
 @pytest.mark.parametrize("key,value", [
-    ("query_param", {"k1": "v1", "k2": "v2", "k3": "v3"}),
-    ("req_header", {"h1": "d1", "h2": "d2", "h3": "d3"}),
+    ("query_params", {"k1": "v1", "k2": "v2", "k3": "v3"}),
+    ("headers", {"h1": "d1", "h2": "d2", "h3": "d3"}),
 ])
 def test_verify_not_strict(key, value, monkeypatch):
-    result = sign_http(alg=ALG, time_stamp=TIME_STAMP, **TEST_DATA)
+    result = sign_http_request(alg=ALG, **TEST_DATA)
     monkeypatch.setitem(TEST_DATA, key, value)
-    verify_http(http_req=result, strict_query_param=False, strict_req_header=False, **TEST_DATA)
+    verify_http_request(signature=result,
+                        strict_query_param_verification=False,
+                        strict_headers_verification=False, **TEST_DATA)
