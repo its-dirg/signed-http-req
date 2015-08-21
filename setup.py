44b9ea233b2e7aa604eafcfba44fea9878462324
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-08-21 11:26:16 +0200

    Prepared setup.py for release.

--- setup.py
+++ setup.py
@@ -9,6 +9,15 @@ setup(
     package_dir={'': 'src'},
     install_requires=['pyjwkest'],
     url='https://github.com/its-dirg/signed-http-req',
+    license='Apache 2.0',
     author='DIRG',
     author_email='dirg@its.umu.se',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
